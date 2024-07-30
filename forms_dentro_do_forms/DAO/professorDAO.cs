679f62a03afec73e33015c8893ca4eb0ee8b7c84
nicolas-peres <nperes441@gmail.com>
2024-07-30 10:50:25 -0300

    bd

---
+++ forms_dentro_do_forms/DAO/professorDAO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using Model.entidades;
+
+namespace MapaSala.DAO
+{
+    public class ProfessorDAO
+    {
+        private string Linhaconexao = "Server = LS05MPF; Database=AULA_DS;User Id = sa; Password=admsasql";
+        private SqlConnection Conexao;
+        public professorDAO()
+        {
+            Conexao = new SqlConnection(Linhaconexao);
+        }
+        public void Inserir(ProfessoresEntidade professor)
+        {
+            Conexao.Open();
+            string query = "Insert into Professores "
+        }
+
+    }
+}
