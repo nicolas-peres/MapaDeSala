c8458b72bbe3ab47e8c620165f6b3a84685d97cb
nicolas-peres <nperes441@gmail.com>
2024-08-05 13:40:39 -0300

    aula com banco de dados

--- forms_dentro_do_forms/DAO/professorDAO.cs
+++ forms_dentro_do_forms/DAO/professorDAO.cs
@@ -13,14 +13,21 @@ namespace MapaSala.DAO
     {
         private string Linhaconexao = "Server = LS05MPF; Database=AULA_DS;User Id = sa; Password=admsasql";
         private SqlConnection Conexao;
-        public professorDAO()
+        public ProfessorDAO()
         {
             Conexao = new SqlConnection(Linhaconexao);
         }
         public void Inserir(ProfessoresEntidade professor)
         {
             Conexao.Open();
-            string query = "Insert into Professores "
+            string query = "Insert into Professores (Nome, Apelido) Values (@nome, @apelido) ";
+            SqlCommand comando = new SqlCommand(query, Conexao);
+            SqlParameter parametro1 = new SqlParameter("@nome", professor.Nome);
+            SqlParameter parametro2 = new SqlParameter("@apelido", professor.Apelido);
+            comando.Parameters.Add(parametro1);
+            comando.Parameters.Add(parametro2);
+            comando.ExecuteNonQuery();
+            Conexao.Close();
         }
 
     }
